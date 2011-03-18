a62752bc8d7b94d9172d75654c14fd3b2d708476
Iwata <iratqq@gmail.com>
2011-03-18 16:28:34 +0900

    fix date

--- theo.h
+++ theo.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010 Iwata <iwata@quasiquote.org>
+ * Copyright (c) 2011 Iwata <iwata@quasiquote.org>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
